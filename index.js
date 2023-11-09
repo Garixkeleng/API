2b8b357423cfdbef9f6214415fa30a452db73f2d
Girish keleng <girishkeleng30@gmail.com>
2023-11-09 12:24:19 +0530

    basic code completed

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+const API_URL = "https://api.genderize.io?";
+
 app.get("/", (req, res)=>{
-    res.render("index.ejs",{name:"Girish keleng"});
+    res.render("index.ejs",{content:"Enter the name"});
+})
+
+app.post("/get-sec", async(req,res)=>{
+    const name = req.body.name;
+    
+    try{
+       const result = await axios.get(API_URL+ "name="+ name);
+       console.log(result);
+    //    const result1 = 
+       res.render("index.ejs",{content:result.data })
+    } catch(error){
+        res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
 })
 
 app.listen(port,(req,res)=>{
